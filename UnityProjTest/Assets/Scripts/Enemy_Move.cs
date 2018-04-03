989c35bff7d1c327a261d68d0ded602b77b74f8d
Ervin <ebatka@aeg1.com>
2018-04-02 20:30:48 -0400

    little billy now destroys red slimes when jumping on them

--- UnityProjTest/Assets/Scripts/Enemy_Move.cs
+++ UnityProjTest/Assets/Scripts/Enemy_Move.cs
@@ -6,15 +6,37 @@ public class Enemy_Move : MonoBehaviour {
 
     public int EnemySpeed;
     public int XMoveDirection;
+	public float height;
+
+	void Start ()
+	{
+		height = GetComponent<SpriteRenderer>().bounds.size.y / 2;
+	}
+
 	
 	// Update is called once per frame
-	void Update () {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
+	void Update () 
+	{
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
-        if (hit.distance < 0.8f && hit.collider != null)
-        {
-            Flip();
-        }
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			if (col.gameObject.transform.position.y > height + gameObject.transform.position.y)
+			{
+				Destroy (gameObject);
+			} 
+			else
+			{
+				Flip();
+			}
+		}
+		else
+		{
+			Flip();
+		}
 	}
 
     void Flip()
@@ -28,4 +50,6 @@ public class Enemy_Move : MonoBehaviour {
             XMoveDirection = 1;
         }
     }
+
+
 }
